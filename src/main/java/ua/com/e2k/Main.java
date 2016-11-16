e9dfd5bb35460ee5d37a1f46f1db8d3b2c8b8959
Mezzya <andrey@cryptex.com.ua>
2016-11-16 12:03:09 +0200

    Initial commit

---
+++ src/main/java/ua/com/e2k/Main.java
@@ -0,0 +1,32 @@
+package ua.com.e2k;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Created by User on 16.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+//        Инициализация базы
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPAMenu");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Menu m1 = new Menu("Шашлык",120,500,false);
+        Menu m2 = new Menu("Борщ", 50,200,true);
+
+        em.persist(m1);
+        em.persist(m2);
+        em.getTransaction().commit();
+
+        System.out.println("Cool");
+
+
+
+
+    }
+}
---
+++ src/main/java/ua/com/e2k/Menu.java
@@ -0,0 +1,68 @@
+package ua.com.e2k;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by User on 16.11.2016.
+ */
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int price;
+    private int weight;
+    private boolean discount;
+
+    public Menu(String name, int price, int weight, boolean discount) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", discount=" + discount +
+                '}';
+    }
+}
